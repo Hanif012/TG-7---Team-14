4da22bfd4a819d3fc3a95853d104806547b8bdfe
QwertTheWert <fredrickwill2005@gmail.com>
2023-11-27 19:52:01 +0700

    Stamina Handler

--- src/PlayerCharacter.gd
+++ src/PlayerCharacter.gd
@@ -2,27 +2,46 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const TIRED_TRESHOLD = 10.0
+const STAMINA_DRAIN = 2
+const STAMINA_REGEN_STILL = 1
+const STAMINA_REGEN_MOVING = 0.2
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var stamina = 200.0
+var tired_flag = false
 
-
-func _physics_process(delta):
-	# Add the gravity.
-#	if not is_on_floor():
-#		velocity.y += gravity * delta
-
-	# Handle Jump.
-#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-#		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+func _process(delta):
+	# Movement controller
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	# Sprint Detection
+	if Input.is_action_pressed("sprint"):
+		if stamina > 0:
+			stamina = max(0, stamina-1)
+			velocity.x *= 2
+			# Apply tiredFlag if drops to 0 stamina
+			if !stamina:
+				tiredFlag = true
+	# Stamina Regeneration
+	else: 
+		# Different stamina regeneration speed based on if you're standing still or not
+		if !velocity.x:
+			stamina = min(240, stamina+0.5)
+		else:
+			stamina = min(240, stamina+0.1)
+		
+		# Remove TiredFlag after a certain stamina treshold
+		if stamina > TIRED_TRESHOLD and tiredFlag:
+			tiredFlag = false
+	
+	# Stop movement if tiredFlag is true
+	if tiredFlag:
+		velocity.x = 0
+	
+	print(stamina, " | ", velocity.x)
 
 	move_and_slide()
